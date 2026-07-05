72bf6909295e6d344b40ba6f372f16b25abd107b
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2026-07-05 19:36:59 +0200

    Refactor String class: enhance functionality with reserve, clear, push_back, and operator overloads

--- projects/string/String.cpp
+++ projects/string/String.cpp
@@ -1,6 +1,9 @@
 #include "String.h"
 #include <cstring>
+#include <cctype>
 #include <algorithm>
+
+//-----------------------PUBLIC--------------------------
 
 String::String(): size_(0), capacity_(0), data_(new char[1]{'\0'}) {}
 
@@ -35,4 +38,86 @@ String::String(std::initializer_list<char> lst): size_(lst.size()), capacity_(si
 
 String::~String() {
     delete[] data_; 
+}
+
+void String::reserve(std::size_t new_cap) {
+    if(capacity_ >= new_cap) {
+        return;
+    }
+    char* new_data = new char[new_cap + 1]; 
+    memcpy(new_data, data_, size_ + 1); 
+    delete[] data_; 
+    data_ = new_data; 
+    capacity_ = new_cap; 
+}
+
+void String::clear() {
+    size_ = 0;
+    data_[0] = '\0';
+}
+
+//Copy and Swap idiom
+String& String::operator=(String other_copy) {
+    swap(other_copy);
+    return *this;
+}
+
+String& String::operator+=(const String& other) {
+    std::size_t new_size = size_ + other.size_; 
+    reserve(std::max(new_size, capacity_ * 2));
+    memcpy(data_ + size_, other.data_, other.size_);
+    size_ = new_size; 
+    data_[size_] = '\0';
+    return *this; 
+}
+
+const char& String::operator[](std::size_t idx) const {
+    return data_[idx];
+}
+char& String::operator[](std::size_t idx) {
+    return data_[idx];
+}
+
+void String::push_back(char c) {
+    if(capacity_ == size_) {
+        reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+    }
+    data_[size_++] = c; 
+    data_[size_] = '\0';
+} 
+
+//-------------------------PRIVATE-------------------------------
+
+void String::swap(String& other) {
+    std::swap(size_, other.size_);
+    std::swap(capacity_, other.capacity_);
+    std::swap(data_, other.data_);
+}
+
+//-----------------------FUNCTIONS-------------------------------
+
+String operator+(String lhs, const String& rhs) {
+    lhs += rhs; 
+    return lhs;
+}
+
+std::ostream& operator<<(std::ostream& out, const String& s) {
+    for(std::size_t i = 0; i < s.size(); ++i) {
+        out << s[i];
+    }
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, String& s) {
+    s.clear();
+    int c = in.get();
+    while(c != EOF && isspace(static_cast<unsigned char>(c))) {
+        c = in.get(); 
+    }
+
+    while(c != EOF && !isspace(static_cast<unsigned char>(c))) {
+        s.push_back(c); 
+        c = in.get(); 
+    }
+    return in;
 }
--- projects/string/String.h
+++ projects/string/String.h
@@ -5,18 +5,42 @@
 #include <iostream>
 
 class String {
-    size_t size_ = 0; 
-    size_t capacity_ = 0; 
+    std::size_t size_ = 0; 
+    std::size_t capacity_ = 0; 
     char* data_ = nullptr;
 public: 
     String();
     String(const char*); 
-    String(size_t, char);
+    String(std::size_t, char);
     explicit String(char);
     String(const String&); 
     String(std::initializer_list<char>);
     ~String();
+
+    String& operator=(String);
+    String& operator+=(const String&); 
+    const char& operator[](std::size_t) const;
+    char& operator[](std::size_t);
+
+    void push_back(char c); 
+    void reserve(std::size_t); 
+    void clear();
+
+    std::size_t size() const {
+      return size_; 
+    }
+
+    bool empty() const {
+      return size_ == 0; 
+    }
+
+private:
+    void swap(String&);
 };
+
+String operator+(String s1, const String& s2); 
+std::ostream& operator<<(std::ostream&, const String&);
+std::istream& operator>>(std::istream&, String&);
 
 #endif
 
