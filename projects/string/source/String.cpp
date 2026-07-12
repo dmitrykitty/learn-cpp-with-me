4319588486ec08b8360f89dd8396b2c322daadfb
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2026-07-12 19:57:44 +0200

    Refactored projekct structure. Added compare operations

---
+++ projects/string/source/String.cpp
@@ -0,0 +1,241 @@
+#include "String.h"
+#include <cstring>
+#include <cctype>
+#include <cassert>
+#include <algorithm>
+
+//-----------------------PUBLIC--------------------------
+
+String::String(): size_(0), capacity_(0), data_(new char[1]{'\0'}) {}
+
+String::String(const char* arr): size_(arr == nullptr ? 0 : strlen(arr)), capacity_(size_), data_(new char[capacity_ + 1]) {
+    data_[size_] = '\0'; 
+    if(arr != nullptr) {
+        memcpy(data_, arr, size_);
+    }
+}
+
+String::String(size_t n, char c): size_(n), capacity_(size_), data_(new char[capacity_ + 1]) {
+    data_[size_] = '\0'; 
+    memset(data_, c, size_); 
+}
+
+String::String(char c): String(1, c) {}
+
+String::String(const String& src): size_(src.size_), capacity_(src.capacity_), data_(new char[capacity_ + 1]) {
+    data_[size_] = '\0';
+    memcpy(data_, src.data_, size_);
+}
+
+String::String(std::initializer_list<char> lst): size_(lst.size()), capacity_(size_), data_(new char[capacity_ + 1]) {
+    data_[size_] = '\0';
+    std::copy(lst.begin(), lst.end(), data_); 
+}
+
+String::~String() {
+    delete[] data_; 
+}
+
+void String::reserve(std::size_t new_cap) {
+    if(capacity_ >= new_cap) {
+        return;
+    }
+    char* new_data = new char[new_cap + 1]; 
+    memcpy(new_data, data_, size_ + 1); 
+    delete[] data_; 
+    data_ = new_data; 
+    capacity_ = new_cap; 
+}
+
+void String::clear() {
+    size_ = 0;
+    data_[0] = '\0';
+}
+
+void String::pop_back() {
+    assert(size_ > 0); 
+    --size_;
+    data_[size_] = '\0';
+}
+
+void String::shrink_to_fit() {
+    if(size_ == capacity_) {
+        return;
+    }
+    char* new_data = new char[size_ + 1];
+    memcpy(new_data, data_, size_ + 1); 
+    delete[] data_; 
+
+    data_ = new_data;
+    capacity_ = size_; 
+}
+
+char& String::back() {
+    assert(size_ > 0);
+    return data_[size_ - 1];
+}
+
+const char& String::back() const {
+    assert(size_ > 0);
+    return data_[size_ - 1];
+}
+
+char& String::front() {
+    assert(size_ > 0);
+    return data_[0];
+}
+
+const char& String::front() const {
+    assert(size_ > 0);
+    return data_[0];
+}
+
+int String::find(const String& substr) const {
+    if (substr.size() == 0) {
+        return 0;
+    }
+    if (substr.size() > size_) {
+        return -1;
+    }
+    for(std::size_t i = 0; i <= size_ - substr.size(); ++i) {
+        std::size_t j = 0;
+        while (j < substr.size() && data_[i + j] == substr[j]) {
+            ++j;
+        }
+        if (j == substr.size()) {
+            return static_cast<int>(i);
+        }
+    }
+    return -1; 
+}
+
+int String::rfind(const String& substr) const {
+    if (substr.empty()) {
+        return static_cast<int>(size_);
+    }
+
+    if (substr.size() > size_) {
+        return -1;
+    }
+
+    std::size_t i = size_ - substr.size();
+
+    while (true) {
+        std::size_t j = 0;
+        while (j < substr.size() && data_[i + j] == substr[j]) {
+            ++j;
+        }
+        if (j == substr.size()) {
+            return static_cast<int>(i);
+        }
+        if (i == 0) {
+            break;
+        }
+        --i;
+    }
+    return -1;
+}
+
+String String::substr(std::size_t left, std::size_t n) const { 
+    String res; 
+    if(left >= size_ || n == 0) {
+        return res; 
+    }
+    std::size_t sz = std::min(n, size_ - left);
+    res.reserve(sz);
+    while(left < size_ && n > 0) {
+        res.push_back(data_[left]); 
+        left++; 
+        n--;
+    }
+    return res;
+}  
+
+//Copy and Swap idiom
+String& String::operator=(String other_copy) {
+    swap(other_copy);
+    return *this;
+}
+
+String& String::operator+=(const String& other) {
+    if (this == &other) {
+        String copy(other);
+        return *this += copy;
+    }
+    
+    std::size_t new_size = size_ + other.size_; 
+    reserve(std::max(new_size, capacity_ * 2));
+    memcpy(data_ + size_, other.data_, other.size_);
+    size_ = new_size; 
+    data_[size_] = '\0';
+    return *this; 
+}
+
+const char& String::operator[](std::size_t idx) const {
+    return data_[idx];
+}
+char& String::operator[](std::size_t idx) {
+    return data_[idx];
+}
+
+bool String::operator==(const String& other) const { 
+    if(size_ != other.size_) {
+        return false; 
+    }
+    return strcmp(data_, other.data_) == 0;
+}
+
+std::strong_ordering String::operator<=>(const String& other) const {
+    int res = strcmp(data_, other.data_); 
+    if(res < 0) {
+        return std::strong_ordering::less; 
+    } 
+    if(res > 0) {
+        return std::strong_ordering::greater; 
+    }
+    return std::strong_ordering::equal; 
+}
+
+void String::push_back(char c) {
+    if(capacity_ == size_) {
+        reserve(capacity_ == 0 ? 1 : capacity_ * 2);
+    }
+    data_[size_++] = c; 
+    data_[size_] = '\0';
+} 
+
+//-------------------------PRIVATE-------------------------------
+
+void String::swap(String& other) noexcept {
+    std::swap(size_, other.size_);
+    std::swap(capacity_, other.capacity_);
+    std::swap(data_, other.data_);
+}
+
+//-----------------------FUNCTIONS-------------------------------
+
+String operator+(String lhs, const String& rhs) {
+    lhs += rhs; 
+    return lhs;
+}
+
+std::ostream& operator<<(std::ostream& out, const String& s) {
+    for(std::size_t i = 0; i < s.size(); ++i) {
+        out << s[i];
+    }
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, String& s) {
+    s.clear();
+    int c = in.get();
+    while(c != EOF && isspace(static_cast<unsigned char>(c))) {
+        c = in.get(); 
+    }
+
+    while(c != EOF && !isspace(static_cast<unsigned char>(c))) {
+        s.push_back(c); 
+        c = in.get(); 
+    }
+    return in;
+}
