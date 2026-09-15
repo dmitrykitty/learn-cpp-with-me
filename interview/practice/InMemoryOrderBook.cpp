95c639af354c20eef0705095e18948d634acfc25
Dmitry_Kitty <dnikitin@student.agh.edu.pl>
2026-09-15 21:55:16 +0200

    InMemoryOrderBook: V2

--- interview/practice/InMemoryOrderBook.cpp
+++ interview/practice/InMemoryOrderBook.cpp
@@ -23,28 +23,43 @@ struct Order {
     int quantity;
 };
 
+struct OrderMeta{
+    Order order;
+    std::list<OrderId>::iterator pos; 
+};
+
 
 class OrderBook {
-    std::map<int, std::set<OrderId>> buy_orders_; 
-    std::map<int, std::set<OrderId>> sell_orders_; 
-    std::unordered_map<OrderId, Order> orders; 
+    std::map<int, std::list<OrderId>> buy_orders_; 
+    std::map<int, std::list<OrderId>> sell_orders_; 
+    std::unordered_map<OrderId, OrderMeta> orders; 
 
-public:
-    void addOrder(const Order& order) {
-        if(order.side == Side::Buy) {
-            buy_orders_[order.price].insert(order.id);
-        } else {
-            sell_orders_[order.price].insert(order.id);
-        }
-        orders[order.id] = order;
+    std::map<int, std::list<OrderId>>& get_orders_by_side(Side side) {
+        return side == Side::Buy ? buy_orders_ : sell_orders_; 
     }
 
+public:
+    //O(logN)
+    void addOrder(const Order& order) {
+        auto& order_map = get_orders_by_side(order.side);
+        auto& lst = order_map[order.price]; //O(logN)
+
+        lst.push_back(order.id); //O(1)
+        auto last_pos = lst.end();
+        last_pos--; 
+
+        orders.emplace(order.id, OrderMeta{.order = order, .pos = last_pos}); //O(1)
+    }
+
+    //O(1)
     std::optional<int> bestBid() const {
         if(!buy_orders_.empty()) {
             return buy_orders_.rbegin()->first;
         }
         return std::nullopt;
     }
+
+    //O(1)
     std::optional<int> bestAsk() const {
         if(!sell_orders_.empty()) {
             return sell_orders_.begin()->first;
@@ -52,20 +67,23 @@ public:
         return std::nullopt;
     }
 
-    //O(logN
+    //O(logN)
     bool cancel_order(OrderId id) {
+        //O(1)
         if(auto order_pos = orders.find(id); order_pos != orders.end()) {
             //assume if order present in orders -> present in buy/sell orders
-            const Order& order = order_pos->second; 
-            auto& order_map = order.side == Side::Buy ? buy_orders_ : sell_orders_; 
+            const OrderMeta& order_meta = order_pos->second; 
+            auto& order_map = order_meta.order.side == Side::Buy ? buy_orders_ : sell_orders_; 
 
-            auto price_pos = order_map.find(order.price);
-            price_pos->second.erase(order.id); 
+            //O(logN)
+            auto price_pos = order_map.find(order_meta.order.price);
+            price_pos->second.erase(order_meta.pos); //O(1)
 
+            //O(1)
             if(price_pos->second.empty()) {
                 order_map.erase(price_pos); 
             }
-            orders.erase(order.id); 
+            orders.erase(order_meta.order.id); //O(1)
             return true;
         } 
         return false;
